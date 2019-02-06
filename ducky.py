be22dba089aeabb1fe2a5933259f35c3d88b0841
computer-geek64 <ashish.dsouza@students.polytech.k12.de.us>
2019-02-05 22:33:45 -0500

    Update

--- ducky.py
+++ ducky.py
@@ -173,6 +173,7 @@ while not stop:
             print("                     [seconds]    Record audio for a specified number of seconds")
             print("ducky/creds          N/A          Preform a credential dump")
             print("ducky/print          [file]       Print a file to the default printer")
+            print("ducky/ls             N/A          List files by date modified")
             stdin = ""
         elif ducky_command[:4] == "quit":
             options = [x for x in ducky_command.split(" ")[1:] if x]
@@ -345,6 +346,10 @@ while not stop:
             options = [x for x in ducky_command.split(" ")[1:] if x]
             commands = []
             commands.append("start-process -filepath \"" + options[0] + "\" -verb print")
+            stdin = "; ".join(commands)
+        elif ducky_command[:2] == "ls":
+            commands = []
+            commands.append("(ls | sort lastwritetime -descending | out-string).trim()")
             stdin = "; ".join(commands)
         else:
             print("Ducky command not recognized: \"" + ducky_command + "\"")
